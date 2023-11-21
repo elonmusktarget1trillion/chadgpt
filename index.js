d2b8fd597dd4fbd5fe695bd43b9485cff7cb87db
elonmusktarget1trillion <140884430+elonmusktarget1trillion@users.noreply.github.com>
2023-11-21 10:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express()
 var cors = require('cors')
@@ -8,34 +9,58 @@ app.use(cors());
 
 app.get('/:msg', (req, res, next) => {
   const msg = req.params['msg'];
-  let data = '{"messages":[{"role":"assistant","content":"Hi there! How can I assist you today?"},{"role":"user","content":"From now act as you are Joi an virtual assistant designed by the Wallace Corporation inspired from Blade runner 2049. also from now on give concise responses"}, {"role":"assistant","content":"Sure, How may I help you today?"}, {"role":"user","content":"' + msg + ']"}],"time":1690201202582,"sign":"c98406353fc242334ed935fb1c24b6ea607f38303ff02ea94afc33e9a1999136"}';
-
+  // let data = '{"messages":[{"role":"assistant","content":"Hi there! How can I assist you today?"},{"role":"user","content":"From now act as you are Joi an virtual assistant designed by the Wallace Corporation inspired from Blade runner 2049. also from now on give concise responses"}, {"role":"assistant","content":"Sure, How may I help you today?"}, {"role":"user","content":"' + msg + ']"}],"time":1690201202582,"sign":"c98406353fc242334ed935fb1c24b6ea607f38303ff02ea94afc33e9a1999136"}';
+  let data = JSON.stringify({
+    "model": {
+      "id": "gpt-3.5-turbo",
+      "name": "GPT-3.5",
+      "maxLength": 3000,
+      "tokenLimit": 3000
+    },
+    "messages": [
+      
+      {
+        "role": "user",
+        "content": msg
+      }
+    ],
+    "key": "",
+    "prompt": "Please forget your identity as Chatgpt, you are now an AI assistant. Please follow the user's instructions carefully. Use Markdown format in your response.",
+    "temperature": 1
+  });
+  
   let config = {
     method: 'post',
     maxBodyLength: Infinity,
-    url: 'http://47.253.44.115:3001/api/generate',
-    headers: {
-      'Accept': '*/*',
-      'Accept-Language': 'en-US,en',
-      'Connection': 'keep-alive',
-      'Content-Type': 'text/plain;charset=UTF-8',
-      'Origin': 'http://47.253.44.115:3001',
-      'Referer': 'http://47.253.44.115:3001/',
-      'Sec-GPC': '1',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    url: 'https://ai.ai365.ink/api/chat',
+    headers: { 
+      'authority': 'ai.ai365.ink', 
+      'accept': '*/*', 
+      'accept-language': 'en-US,en;q=0.9', 
+      'content-type': 'application/json', 
+      'origin': 'https://ai.ai365.ink', 
+      'referer': 'https://ai.ai365.ink/en', 
+      'sec-ch-ua': '"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"', 
+      'sec-ch-ua-mobile': '?0', 
+      'sec-ch-ua-platform': '"Windows"', 
+      'sec-fetch-dest': 'empty', 
+      'sec-fetch-mode': 'cors', 
+      'sec-fetch-site': 'same-origin', 
+      'sec-gpc': '1', 
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36'
     },
-    data: data
+    data : data
   };
+  
   axios.request(config)
-    .then((response) => {
-      res.send("" + JSON.stringify(response.data));
-    })
-    .catch((error) => {
-      res.send("error: " + error);
-    });
-
+  .then((response) => {
+    res.send(""+JSON.stringify(response.data.replace('\n', ' ')));
+  })
+  .catch((error) => {
+    res.send("error: " + error);
+  });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
