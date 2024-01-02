03a9bdbff29ac409bbf2c35cbeb1a2aff3936a3e
elonmusktarget1trillion <140884430+elonmusktarget1trillion@users.noreply.github.com>
2024-01-02 19:40:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ myHeaders.append("x-same-domain", "1");
     return AIResponse
 };
 
-app.get('/:msg', (req, res, next) => {
+app.get('/:msg', async (req, res, next) => {
   const msg = req.params['msg'];
   // let data = '{"messages":[{"role":"assistant","content":"Hi there! How can I assist you today?"},{"role":"user","content":"From now act as you are Joi an virtual assistant designed by the Wallace Corporation inspired from Blade runner 2049. also from now on give concise responses"}, {"role":"assistant","content":"Sure, How may I help you today?"}, {"role":"user","content":"' + msg + ']"}],"time":1690201202582,"sign":"c98406353fc242334ed935fb1c24b6ea607f38303ff02ea94afc33e9a1999136"}';
   const promptText  = msg;
