5df68dd834ee6cedf6bee9f8a78abb933185cf44
elonmusktarget1trillion <140884430+elonmusktarget1trillion@users.noreply.github.com>
2024-01-02 20:48:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ app.use(cors());
 app.get('/:msg', async (req, res, next) => {
   const msg = req.params['msg'];
   // let data = '{"messages":[{"role":"assistant","content":"Hi there! How can I assist you today?"},{"role":"user","content":"From now act as you are Joi an virtual assistant designed by the Wallace Corporation inspired from Blade runner 2049. also from now on give concise responses"}, {"role":"assistant","content":"Sure, How may I help you today?"}, {"role":"user","content":"' + msg + ']"}],"time":1690201202582,"sign":"c98406353fc242334ed935fb1c24b6ea607f38303ff02ea94afc33e9a1999136"}';
- const axios = require('axios');
-
 const options = {
   method: 'POST',
   url: 'https://text-generation-chatgpt.p.rapidapi.com/Chat',
@@ -24,7 +22,7 @@ const options = {
     messages: [
       {
         role: 'user',
-        content: 'who are you'
+        content: msg
       }
     ],
     max_tokens: 2500
