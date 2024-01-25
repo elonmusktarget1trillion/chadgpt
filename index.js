180d2fad4296bfe61eaaf9390368410a2ac3b320
elonmusktarget1trillion <140884430+elonmusktarget1trillion@users.noreply.github.com>
2024-01-25 08:54:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,14 @@ app.get('/:msg', async (req, res, next) => {
   // let data = '{"messages":[{"role":"assistant","content":"Hi there! How can I assist you today?"},{"role":"user","content":"From now act as you are Joi an virtual assistant designed by the Wallace Corporation inspired from Blade runner 2049. also from now on give concise responses"}, {"role":"assistant","content":"Sure, How may I help you today?"}, {"role":"user","content":"' + msg + ']"}],"time":1690201202582,"sign":"c98406353fc242334ed935fb1c24b6ea607f38303ff02ea94afc33e9a1999136"}';
 const options = {
   method: 'POST',
-  url: 'https://text-generation-chatgpt.p.rapidapi.com/Chat',
+  url: 'https://chatgpt-gpt5.p.rapidapi.com/ask',
   headers: {
     'content-type': 'application/json',
-    'X-RapidAPI-Key': 'ede3c5163fmsh01abdacf07fd2b0p1c0e4bjsn1db1b15be576',
-    'X-RapidAPI-Host': 'text-generation-chatgpt.p.rapidapi.com'
+    'X-RapidAPI-Key': 'aed7821064mshd2b61cc8cf343b1p1bf5a2jsn79c55d3b28cc',
+    'X-RapidAPI-Host': 'chatgpt-gpt5.p.rapidapi.com'
   },
   data: {
-    messages: [
-      {
-        role: 'user',
-        content: msg
-      }
-    ],
-    max_tokens: 2500
+    query: msg
   }
 };
 
